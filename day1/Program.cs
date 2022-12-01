4ca4f2f46ee0bbe8b8608de3352eb7b29157d7a2
Dylan Beattie <dylan@dylanbeattie.net>
2022-12-01 16:10:33 +0000

    Formatting.

--- day1/Program.cs
+++ day1/Program.cs
@@ -1,14 +1,12 @@
 ﻿var elves = File.ReadAllText("input.txt")
-.Split("\n\n")
-.Select(chunk => 
-    chunk
-        .Split("\n", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
-        .Select(Int32.Parse).Sum());
+    .Split("\n\n")
+    .Select(chunk => 
+        chunk
+            .Split("\n", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+            .Select(Int32.Parse).Sum());
 
 var part1 = elves.Max();
 Console.WriteLine(part1);
 
-
 var part2 = elves.OrderByDescending(calories => calories).Take(3).Sum();
-
 Console.WriteLine(part2);
