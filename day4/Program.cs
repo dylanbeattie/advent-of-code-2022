0f34f7079d67ea904d56014bfabb256b552781ef
Dylan Beattie <dylan@dylanbeattie.net>
2022-12-04 11:50:29 +0000

    Day 4. That was nice.

---
+++ day4/Program.cs
@@ -0,0 +1,16 @@
+﻿var inputs = File.ReadAllLines("input.txt")
+	.Select(line => line.Split(',', '-').Select(Int32.Parse).ToArray())
+	.Select(Chunkify);
+
+Console.WriteLine($"Part 1: {inputs.Where(Includes).Count()}");
+Console.WriteLine($"Part 2: {inputs.Where(Overlaps).Count()}");
+
+IEnumerable<int>[] Chunkify(int[] ints)
+    => ints.Chunk(2).Select(pair => Enumerable.Range(pair[0], pair[1] - pair[0] + 1)).ToArray();
+
+bool Includes(IEnumerable<int>[] ranges) {
+	var intersectSize = ranges[0].Intersect(ranges[1]).Count();
+	return (intersectSize == ranges[0].Count() || intersectSize == ranges[1].Count());
+}
+
+bool Overlaps(IEnumerable<int>[] ranges) => ranges[0].Intersect(ranges[1]).Any();
