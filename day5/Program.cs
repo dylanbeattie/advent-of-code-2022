9abf9a750cd10a4184da37b2f640c5623d5355ac
Dylan Beattie <dylan@dylanbeattie.net>
2022-12-05 23:14:42 +0000

    OK, that was a bit fruity. Kinda fun though.

--- day5/Program.cs
+++ day5/Program.cs
@@ -1,44 +1,41 @@
-﻿(Stack<char>[], IEnumerable<(int,int,int)>) ReadInputs(string filename) {
-    var inputs = File.ReadAllLines(filename);
+﻿var inputs = File.ReadAllLines("input.txt");
+var crates = inputs.TakeWhile(line => line != "")
+	.Select(line => line.Chunk(4).Select(chars => chars[1]).ToArray())
+	.Reverse().ToArray();
 
-    var stackLines = inputs.TakeWhile(line => line != "")
-        .Select(line => line.Chunk(4).Select(chunk => chunk[1]).ToArray())
-        .Reverse().ToArray();
+var moves = inputs.SkipWhile(line => line != "").Skip(1)
+	.Select(line => line.Split(' '))
+	.Select(tokens => new[] { tokens[1], tokens[3], tokens[5] }.Select(Int32.Parse).ToArray())
+	.Select(ints => (ints[0], ints[1] - 1, ints[2] - 1));
 
-    var stacks = stackLines[0].Select(_ => new Stack<char>()).ToArray();
-    foreach(var line in stackLines[1..]) {
-        for(var i = 0; i < stacks.Length; i++) if (line[i] != ' ') stacks[i].Push(line[i]);
+var stacks1 = crates[0].Select(_ => new Stack<char>()).ToArray();
+foreach (var line in crates[1..]) {
+	for (var i = 0; i < stacks1.Length; i++) {
+        if (line[i] != ' ') stacks1[i].Push(line[i]);
     }
-
-    var moves = inputs.SkipWhile(line => line != "").Skip(1)
-        .Select(line => line.Split(' '))
-        .Select(tokens => new[] { tokens[1], tokens[3], tokens[5] }.Select(Int32.Parse).ToArray())
-        .Select(ints => (ints[0],ints[1]-1,ints[2]-1));
-
-    return (stacks, moves);
 }
-var filename = "input.txt";
-var (stacks, moves) = ReadInputs(filename);
-foreach(var (count,source,target) in moves) {
-    var i = 0;
-    while (i++ < count) stacks[target].Push(stacks[source].Pop());
+
+var stacks2 = stacks1.Select(t => new Stack<char>(t.Reverse())).ToArray();
+
+foreach (var (count, source, target) in moves) {
+	var i = 0;
+	while (i++ < count) stacks1[target].Push(stacks1[source].Pop());
 }
-var part1 = new String(stacks.Select(stack => stack.Peek()).ToArray());;
+var part1 = new String(stacks1.Select(stack => stack.Peek()).ToArray()); ;
 Console.WriteLine($"Solution to part 1: {part1}");
 
-(stacks, moves) = ReadInputs(filename);
-foreach(var (count,source,target) in moves) {
-    stacks[target].Push(stacks[source].Pop(count));
+foreach (var (count, source, target) in moves) {
+    stacks2[target].Push(stacks2[source].Pop(count));
 }
 
-var part2 = new String(stacks.Select(stack => stack.Peek()).ToArray());;
+var part2 = new String(stacks2.Select(stack => stack.Peek()).ToArray()); ;
 Console.WriteLine($"Solution to part 2: {part2}");
 
 static class StackExtensions {
-    public static IEnumerable<T> Pop<T>(this Stack<T> stack, int count) {
-        for(var i = 0; i < count; i++) yield return stack.Pop();
-    }
-    public static void Push<T>(this Stack<T> stack, IEnumerable<T> items) {
-        foreach(var item in items.Reverse()) stack.Push(item);
-    }
+	public static IEnumerable<T> Pop<T>(this Stack<T> stack, int count) {
+		for (var i = 0; i < count; i++) yield return stack.Pop();
+	}
+	public static void Push<T>(this Stack<T> stack, IEnumerable<T> items) {
+		foreach (var item in items.Reverse()) stack.Push(item);
+	}
 }
